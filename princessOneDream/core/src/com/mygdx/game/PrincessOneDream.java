08f57edf6bccb8412818ba61d845641132d24bcb
DarioSousa <justtoofnhotmail.com>
2019-04-25 18:15:21 +0000

    collisions done

--- princessOneDream/core/src/com/mygdx/game/PrincessOneDream.java
+++ princessOneDream/core/src/com/mygdx/game/PrincessOneDream.java
@@ -3,8 +3,11 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -18,11 +21,17 @@ public class PrincessOneDream extends ApplicationAdapter {
     private boolean isFalling;
     private float jumpDistance;
     private float maxJumpHeight;
+    private String text;
+    private BitmapFontCache cache;
+    private BitmapFont font;
+
+    private Rectangle animalRec;
+    private Texture animalTex;
 
     private Texture background;
     private int backgroundX;
 
-    public PrincessOneDream(){
+    public PrincessOneDream() {
         isFalling = false;
         isJumping = false;
         jumpDistance = 5;
@@ -32,29 +41,41 @@ public class PrincessOneDream extends ApplicationAdapter {
 
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        img = new Texture("walking3.png");
+        animalTex = new Texture("cow2.png");
+
+        font = new BitmapFont();
+        text = "";
+        cache = font.newFontCache();
+
+        animalRec = new Rectangle();
+        animalRec.x = 400;
+        animalRec.y = 0;
+        animalRec.width = animalTex.getWidth();
+        animalRec.height = animalTex.getHeight();
 
         player = new Rectangle();
         player.x = 0;
         player.y = 0;
-        player.width = 50;
-        player.height = 150;
+        player.width = img.getWidth();
+        player.height = img.getHeight();
 
         background = new Texture("forestBuilt.jpeg");
         backgroundX = 0;
 
-	}
+    }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background,backgroundX,0);
+        batch.draw(background, backgroundX, 0);
         batch.draw(img, player.x, player.y);
-        batch.end();
-        jump();
+        batch.draw(animalTex, animalRec.x, animalRec.y);
         userInputs();
+        jump();
+        batch.end();
     }
 
     @Override
@@ -108,6 +129,15 @@ public class PrincessOneDream extends ApplicationAdapter {
             }
         }
 
+        if (player.overlaps(animalRec) ) {
+            if (player.x < animalRec.x){
+                player.x = animalRec.getX() - player.width;
+            } else {
+                player.x = animalRec.getX() + animalRec.width;
+            }
+            renderSingleLine();
+        }
+
         if (player.x < 0) {
             player.x = 0;
         }
@@ -119,30 +149,36 @@ public class PrincessOneDream extends ApplicationAdapter {
     }
 
     public void jump() {
-        if(!isJumping){
+        if (!isJumping) {
             return;
         }
         player.y += jumpDistance;
 
-        if(!isFalling){
-            jumpDistance -=0.1;
-        } else{
+        if (!isFalling) {
+            jumpDistance -= 0.1;
+        } else {
             jumpDistance += 0.1;
         }
 
-        if(!isFalling && player.y >= maxJumpHeight){
+        if (!isFalling && player.y >= maxJumpHeight) {
             isFalling = true;
             jumpDistance *= -1;
         }
-
-        if(isFalling && player.y <= 0){
-            System.out.println("AAA");
+        if (player.y < 1 && isFalling) {
             player.y = 0;
             isFalling = false;
             isJumping = false;
 
         }
 
+    }
+
+    private void renderSingleLine() {
+        text = "Daniel é gay";
+        cache.setText(text, animalRec.getX() + 10, animalRec.getY() + animalRec.height + 35);
+        cache.setColors(Color.BLACK);
+
+        cache.draw(batch);
     }
 
 }
